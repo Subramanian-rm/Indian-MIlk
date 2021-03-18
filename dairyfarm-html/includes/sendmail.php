7eefc7fafecf1934f260252124d3a8dc3dd8cd9f
Subramanian-rm <79912945+Subramanian-rm@users.noreply.github.com>
2021-03-18 19:14:33 +0530

    gggg

---
+++ dairyfarm-html/includes/sendmail.php
@@ -0,0 +1,40 @@
+<?php 
+global $_REQUEST;
+$response = array('error'=>'');
+$contact_email = 'subburama14@gmail.com';
+
+// type
+$type = $_REQUEST['type'];
+print_r($type);die;	
+// parse
+parse_str($_POST['data'], $post_data);	
+		
+
+		$user_name = stripslashes(strip_tags(trim($post_data['username'])));
+		$user_email = stripslashes(strip_tags(trim($post_data['email'])));
+		$user_mobile = stripslashes(strip_tags(trim($post_data['sc_form_mobile'])));
+		$user_subject = stripslashes(strip_tags(trim($post_data['subject'])));
+		$user_msg =stripslashes(strip_tags(trim( $post_data['message'])));
+			
+		if (trim($contact_email)!='') {
+			$subj = 'Message from Dairy Farm';
+			$msg = $subj." \r\nName: $user_name \r\nE-mail: $user_email \r\nMobile Number: $user_mobile \r\nSubject: $user_subject \r\nMessage: $user_msg";
+		
+			$head = "Content-Type: text/plain; charset=\"utf-8\"\n"
+				. "X-Mailer: PHP/" . phpversion() . "\n"
+				. "Reply-To: $user_email\n"
+				. "To: $contact_email\n"
+				. "From: $user_email\n";
+		echo ' send message!';
+			if (!@mail($contact_email, $subj, $msg, $head)) {
+				$response['error'] = 'Error send message!';
+			}
+		} else 
+				$response['error'] = 'Error send message!';	
+		
+		
+
+	//echo json_encode($post_data['username'].''.$post_data['email'].''$post_data['subject'].''.$post_data['message']);	
+	echo json_encode($response);
+	die();
+?>
